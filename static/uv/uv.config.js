c62dca4f31a136fb0d92ba5e5391b84a32dd8569
zachywacky2 <102396917+zachywacky2@users.noreply.github.com>
2024-05-28 23:02:05 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: '/static/glacier/',
     bare:'https://juniorhighmaths.tafca.co.uk/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
