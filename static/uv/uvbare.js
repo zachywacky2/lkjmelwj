beedf2589228060920a9416e1a040d616b6cfd70
zachywacky2 <102396917+zachywacky2@users.noreply.github.com>
2024-06-10 20:28:14 -0600

    Update uvbare.js

--- static/uv/uvbare.js
+++ static/uv/uvbare.js
@@ -1,1 +1,10 @@
-
+self.__uv$config = {
+    prefix: '/static/glacier/',
+    bare:'https://bareglacieer.global.ssl.fastly.net/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
