408f364bf6be952be8b055df55651e66d8cc7936
zachywacky2 <102396917+zachywacky2@users.noreply.github.com>
2024-08-01 17:42:13 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/glacier/',
-    bare:'https://loldoe.global.ssl.fastly.net/bare/',
+    bare:'https://badreglacier.global.ssl.fastly.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
