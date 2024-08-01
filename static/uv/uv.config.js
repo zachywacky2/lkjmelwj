189782c1eb10cf482301ee7fe32bfb04e8411ce8
zachywacky2 <102396917+zachywacky2@users.noreply.github.com>
2024-08-01 01:00:32 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/glacier/',
-    bare:'https://phantom.lol/bare/',
+    bare:'https://1111rw.global.ssl.fastly.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
