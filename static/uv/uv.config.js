409659fe40f3d9edb5a04338ea086b4bc1296111
zachywacky2 <102396917+zachywacky2@users.noreply.github.com>
2025-08-17 16:21:23 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/glacier/',
-    bare:'https://skbiditoi.lat/tspmo/',
+    bare:'https://jesus.greenlifechurch.org/b/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
